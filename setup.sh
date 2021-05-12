f57391d00d57e90f5209fd2ff757b64325b3e50f
Matheus Correia <lucfcor@hotmail.com>
2021-05-12 20:17:31 -0300

    chore: bash to init api

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#date
+#author
+#purpose
+
+mkdir ./dynamolocal
+wget http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_latest.tar.gz
+tar xzf dynamodb_local_latest.tar.gz
+rm -f dynamodb_local_latest.tar.gz
+java -Djava.library.path=./DynamoDBLocal_lib/ -jar DynamoDBLocal.jar
+
+npm run build
+npm run setup
+npm run start
+
+#END
